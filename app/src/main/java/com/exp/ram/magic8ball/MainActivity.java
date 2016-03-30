610fe9a09824c95cf033d559deab043903e1147b
ramteen <happinin@gmail.com>
2016-03-30 14:57:25 +1100

    added keyboard functionality

--- app/src/main/java/com/exp/ram/magic8ball/MainActivity.java
+++ app/src/main/java/com/exp/ram/magic8ball/MainActivity.java
@@ -1,10 +1,12 @@
 package com.exp.ram.magic8ball;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -15,7 +17,7 @@ public class MainActivity extends AppCompatActivity {
     TextView label;
     EditText textfield;
     String answerArray[] = {"Yes", "No", "Maybe", "Whatever", "Reply hazy, try again"};
-
+    String answerArray2[] = {"Yes", "No", "Maybe", "Whatever", "Reply hazy, try again"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +33,27 @@ public class MainActivity extends AppCompatActivity {
 
 
         label.setText("Magic (8)");
+
+
+        textfield.setOnKeyListener(new View.OnKeyListener() {
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                // If the event is a key-down event on the "enter" button
+                if ((event.getAction() == KeyEvent.ACTION_DOWN) && (keyCode == KeyEvent.KEYCODE_ENTER)) {
+                    // Perform action on key press
+                    textfield.setText("");
+                    int idx = new Random().nextInt(answerArray2.length);
+                    String random = (answerArray2[idx]);
+                    label.setText(random);
+
+                    InputMethodManager in = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                    in.hideSoftInputFromWindow(textfield.getApplicationWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
+
+                    return true;
+                }
+                return false;
+            }
+        });
+
 
 
     }
