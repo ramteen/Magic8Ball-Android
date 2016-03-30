20cd882822bf840995a1be3e32fb83a54dedce1b
ramteen <happinin@gmail.com>
2016-03-30 15:18:54 +1100

    changes images and added animation

--- app/src/main/java/com/exp/ram/magic8ball/MainActivity.java
+++ app/src/main/java/com/exp/ram/magic8ball/MainActivity.java
@@ -5,19 +5,22 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.View;
-import android.view.inputmethod.EditorInfo;
+import android.view.animation.AlphaAnimation;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView label;
+    ImageView pic;
     EditText textfield;
     String answerArray[] = {"Yes", "No", "Maybe", "Whatever", "Reply hazy, try again"};
     String answerArray2[] = {"Yes", "No", "Maybe", "Whatever", "Reply hazy, try again"};
+    String files[] = {"circle1", "circle2", "circle3", "circle4", "circle5", "circle6"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,7 +33,7 @@ public class MainActivity extends AppCompatActivity {
         Button sb = (Button) findViewById(R.id.sb);
         label = (TextView) findViewById(R.id.textView);
         textfield = (EditText) findViewById(R.id.editText);
-
+        pic = (ImageView) findViewById(R.id.pic);
 
         label.setText("Magic (8)");
 
@@ -43,10 +46,21 @@ public class MainActivity extends AppCompatActivity {
                     textfield.setText("");
                     int idx = new Random().nextInt(answerArray2.length);
                     String random = (answerArray2[idx]);
-                    label.setText(random);
+
+                    AlphaAnimation animation1 = new AlphaAnimation(0.0f, 1.0f);
+                    animation1.setDuration(1000);
+
 
                     InputMethodManager in = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
                     in.hideSoftInputFromWindow(textfield.getApplicationWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
+
+                    idx = new Random().nextInt(files.length);
+                    String filename = files[idx];
+                    pic.setImageResource(getResources().getIdentifier(filename, "drawable", getPackageName()));
+
+                    label.setText(random);
+                    label.startAnimation(animation1);
+
 
                     return true;
                 }
